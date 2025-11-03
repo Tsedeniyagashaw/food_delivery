7efadcadc72c733c33f7f861b07d2b6c62586535
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2025-11-03 21:15:11 +0300

    updates

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,66 +3,67 @@ import React from 'react'
 
 const CartPage = () => {
   return (
-    <div className='h-[calc(100vh-6rem)]  md:h-[calc(100vh-9rem)] flex flex-col text-orange-600 overflow-scroll'>
-     
-     
-     
-      <div className='h-1/2 p-4 flex flex-col items-center'>
-
-        <div className='flex items-center justify-between mb-4'>
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col lg:flex-row text-orange-600 overflow-scroll">
+      
+      <div className="h-1/2 p-4 flex flex-col justify-center items-center
+       gap-4 lg:h-full lg:w-2/3  2xl:w-1/2 lg:px-20 xl:px-40">
+        
+        <div className="flex items-center justify-between w-full ">
           <Image src="/Group 16.png" alt="" width={100} height={100} />
-          <div className=''>
-        <h1 className='uppercase text-xl font-bold'>Sicilian</h1>
-        <span>Large</span>
+          <div className="flex flex-col items-start">
+            <h1 className="uppercase text-xl font-bold">Sicilian</h1>
+            <span>Large</span>
           </div>
-          <h2 className='font-bold'>79.97</h2>
-          <span className=' cursor-pointer'>X</span>
+          <h2 className="font-bold">$79.97</h2>
+          <span className="cursor-pointer">X</span>
         </div>
-    
 
-        <div className='h-1/2 p-4 flex flex-col items-center'>
-
-        <div className='flex items-center justify-between mb-4'>
+        <div className="flex items-center justify-between w-full  pb-2">
           <Image src="/Group 16.png" alt="" width={100} height={100} />
-          <div className=''>
-        <h1 className='uppercase text-xl font-bold'>Sicilian</h1>
-        <span>Large</span>
+          <div className="flex flex-col items-start">
+            <h1 className="uppercase text-xl font-bold">Sicilian</h1>
+            <span>Large</span>
           </div>
-          <h2 className='font-bold'>79.97</h2>
-          <span className=' cursor-pointer'>X</span>
+          <h2 className="font-bold">$79.97</h2>
+          <span className="cursor-pointer">X</span>
         </div>
-      </div>
 
-        <div className='h-1/2 p-4 flex flex-col items-center '>
-
-        <div className='flex items-center justify-between mb-4'>
+        <div className="flex items-center justify-between w-full ">
           <Image src="/Group 16.png" alt="" width={100} height={100} />
-          <div className=''>
-        <h1 className='uppercase text-xl font-bold'>Sicilian</h1>
-        <span>Large</span>
+          <div className="flex flex-col items-start">
+            <h1 className="uppercase text-xl font-bold">Sicilian</h1>
+            <span>Large</span>
           </div>
-          <h2 className='font-bold'>79.97</h2>
-          <span className=' cursor-pointer'>X</span>
+          <h2 className="font-bold">$79.97</h2>
+          <span className="cursor-pointer">X</span>
         </div>
       </div>
 
-      <div className='h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center'>
-        <div className='flex justify-between'>
+      <div className="p-4 bg-fuchsia-50 flex flex-col  lg:px-20 xl:px-40 gap-4 justify-center  lg:h-full lg:w-1/3 2xl:w-1/2 2xl:text-xl 2xl:gap-46">
+        <div className="flex justify-between">
           <span>Subtotal (3 items)</span>
           <span>$180.60</span>
         </div>
-        <hr />  <div className='flex justify-between'>
+     
+        <div className="flex justify-between">
           <span>Service cost</span>
           <span>$23.60</span>
         </div>
-        <hr />  <div className='flex justify-between'>
-          <span>Delivery cost</span>
-          <span className='text-green-500'>FREE</span>
-        </div>
-        <hr />
-     <button className='bg-orange-600 text-white p-3 rounded-md'>CHECKOUT</button>
 
-      </div>
+        <div className="flex justify-between">
+          <span>Delivery cost</span>
+          <span className="text-green-500">FREE</span>
+        </div>
+        <hr className='my-2' />
+          <div className="flex justify-between">
+          <span>TOTAL(INCL. VAT)</span>
+          <span className="font-bold">$214.20</span>
+        </div>
+      <button className="bg-orange-600 text-white py-3 px-4 rounded-md
+       font-semibold w-full sm:w-auto  sm:self-end hover:bg-orange-700
+        transition self-end">
+          CHECKOUT
+        </button> </div>
 
     </div>
   )
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
+import { FaGoogle, FaFacebook } from "react-icons/fa";
+
 
 const LoginPage = () => {
   return (
-    <div>
-      
+    <div className='p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-6rem)] flex items-center justify-center'>
+
+    <div className='h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2'>
+      <div className='relative h-1/3 w-full md:h-full md:w-1/2'>
+    <Image src="/Group 8.png" alt='' fill className='object-cover'/>
+      </div>
+
+
+      <div className='p-10 flex flex-col gap-8 md:w-1/2'>
+        <h1 className='font-bold text-xl xl:text-2xl'>WELCOME</h1>
+        <p>Log into your account or create a new one using social buttons</p>
+      <button className='flex gap-4  p-4 ring-1 ring-orange-100 rounded-md'>  <FaGoogle size={20} color='blue'/> Sign in with Google</button>
+      <button className='flex gap-4  p-4 ring-1 ring-orange-100 rounded-md'>   <FaFacebook size={20} color='blue'/> Sign in with Facabook</button>
+      <p className='text-sm'>Have a problem? <Link className='underline' href="/">Contact Us</Link></p>
+      </div>
+      </div>  
     </div>
   )
 }
