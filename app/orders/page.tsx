ab4c12ed275624bb83cdb897fbcabb6ad4192b46
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2025-11-03 21:45:21 +0300

    updates

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -2,7 +2,56 @@ import React from 'react'
 
 const OrdersPage = () => {
   return (
-    <div>
+    <div className='p-4 lg:px-20 xl:px-40'>
+      <table className='w-full border-separate'>
+        <thead>
+          <tr className='text-left'>
+      <th className='hidden md:block'>Order Id</th>
+      <th>Date</th>
+      <th>Price</th>
+      <th className='hidden md:block'>Products</th>
+      <th>Status</th></tr>
+        </thead>
+
+        <tbody>
+          <tr className='text-sm md:text-base bg-red-50'>
+      <td className='hidden md:block py-6 px-1'>134253465</td>
+      <td className=' py-6 px-1'>12.12.2022</td>
+      <td className=' py-6 px-1'>$25</td>
+      <td className='hidden md:block  py-6 px-1'>Big burger Menu(2), pizza, coca cola</td>
+      <td className=' py-6 px-1'>on the way</td>
+</tr>
+
+         <tr className='text-sm md:text-base odd:bg-gray-100'>
+      <td className='hidden md:block py-6 px-1'>134253465</td>
+      <td className=' py-6 px-1'>12.12.2022</td>
+      <td className=' py-6 px-1'>$25</td>
+      <td className='hidden md:block  py-6 px-1'>Big burger Menu(2), pizza, coca cola</td>
+      <td className=' py-6 px-1 text-green-400'>Delivered</td>
+</tr>
+       <tr className='text-sm md:text-base odd:bg-gray-100'>
+      <td className='hidden md:block py-6 px-1'>134253465</td>
+      <td className=' py-6 px-1'>12.12.2022</td>
+      <td className=' py-6 px-1'>$25</td>
+      <td className='hidden md:block  py-6 px-1'>Big burger Menu(2), pizza, coca cola</td>
+      <td className=' py-6 px-1 text-green-400'>Delivered</td>
+</tr>
+       <tr className='text-sm md:text-base odd:bg-gray-100'>
+      <td className='hidden md:block py-6 px-1'>134253465</td>
+      <td className=' py-6 px-1'>12.12.2022</td>
+      <td className=' py-6 px-1'>$25</td>
+      <td className='hidden md:block  py-6 px-1'>Big burger Menu(2), pizza, coca cola</td>
+      <td className=' py-6 px-1 text-green-400'>Delivered</td>
+</tr>
+       <tr className='text-sm md:text-base odd:bg-gray-100'>
+      <td className='hidden md:block py-6 px-1'>134253465</td>
+      <td className=' py-6 px-1'>12.12.2022</td>
+      <td className=' py-6 px-1'>$25</td>
+      <td className='hidden md:block  py-6 px-1'>Big burger Menu(2), pizza, coca cola</td>
+      <td className=' py-6 px-1 text-green-400'>Delivered</td>
+</tr>
+        </tbody>
+      </table>
       
     </div>
   )
