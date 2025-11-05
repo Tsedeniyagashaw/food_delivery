7aaf1d3677f48f2f96d5a57486122f4f128589f5
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2025-11-05 18:24:58 +0300

    updates

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -4,28 +4,40 @@ import { featuredItems } from '../data'
 
 const Featured = () => {
   return (
-    <div className='w-screen overflow-x-scroll text-orange-500 '>
-     <div className='w-max flex'>
+    <div className="w-full overflow-x-scroll scrollbar-hide text-orange-500 scroll-smooth snap-x snap-mandatory">
+      <div className="flex w-max">
+        {featuredItems.map((item) => (
+          <div
+            key={item.id}
+            className="shrink-0 h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-orange-50 transition-all
+            duration-300 w-[80vw] md:w-[45vw] xl:w-[30vw] xl:h-[90vh] snap-center"
+          >
+            <div className="relative flex-1 w-full hover:rotate-60 transition-all duration-500">
+              <Image
+                src={item.image}
+                alt={item.title}
+                fill
+                quality={100}
+                sizes="(max-width: 768px) 80vw,
+                       (max-width: 1200px) 45vw,
+                       30vw"
+                className="object-contain"
+              />
+            </div>
 
-      {featuredItems.map(item =>(
-        
-        <div key={item.id} className='w-screen h-[60vh] flex flex-col 
-        items-center justify-around p-4 hover:bg-gray-100 transition-all
-         duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]'>
-        <div className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500 '>
-          <Image src={item.image} alt="" fill className='object-contain'/>
-        </div>
-
-        <div className='flex-1 flex flex-col items-center justify-center text-center gap-4 '>
-          <h1 className='text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl'>{item.title}</h1>
-          <p className='p-4'>{item.desc}</p>
-          <span className='text-xl font-bold'>{item.price}</span>
-          <button className='bg-orange-600 text-white p-2 rounded-md'>Add to Cart</button>
-        </div>
-
-      </div>  ) )}
-
-     </div>
+            <div className="flex-1 flex flex-col items-center justify-center text-center gap-4">
+              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">
+                {item.title}
+              </h1>
+              <p className="p-4">{item.desc}</p>
+              <span className="text-xl font-bold">${item.price}</span>
+              <button className="bg-orange-600 text-white p-2 rounded-md hover:bg-orange-700 transition">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='h-12 md:h-24 px-4 lg:px-20 xl:p-40  text-orange-600 flex items-center justify-center'>
+    <div className='h-12 md:h-24 px-4 lg:px-20 xl:px-40  text-orange-600 flex items-center justify-center'>
       <Link href="/">BurgerHub | </Link>
       <p> &copy; ALL RIGHT RESERVED</p>
     </div>
